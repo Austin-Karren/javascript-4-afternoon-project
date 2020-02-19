77a3247d6797108685e7183cebe0d1bca2cb4405
Austin-Karren <austin.t.karren@gmail.com>
2020-02-19 14:47:31 -0700

    second commit

--- context.js
+++ context.js
@@ -17,11 +17,12 @@
   email --> which is a string
   getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 */
-  
 
-//Code Here
-
-
+const user = {username: '', email: '', 
+  getUsername: function (){
+    return this.username;
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -41,7 +42,7 @@ class Animal {
   }
 }
 
-//Code Here
+const animal1 = new Animal('Bambi', 'Deer', 'Bushes');
 
 
 
@@ -61,8 +62,7 @@ let who = {
   location: 'Belize'
 }
 
-//Code Here
-
+const whoSaysHi = sayHi.bind(who);
 
 
 ////////// PROBLEM 4 //////////
@@ -72,11 +72,11 @@ let who = {
 */
 
 function whatIsThis() {
-  return this
+  return this;
 }
 
 // uncomment the line below and tell us what the context of "this" is for whatIsThis()
-//let context1 = ???
+let context1 = whatIsThis();
 
 let product = {
   name: 'snake plant',
@@ -87,7 +87,7 @@ let product = {
 let func = whatIsThis.bind(product)
 
 // uncomment the line below and tell us what the context of "this" is when we invoke func
-//let context2 = ???
+let context2 = func();
 
 let vacation = {
   location: 'Hawaii',
@@ -98,7 +98,7 @@ let vacation = {
 }
 
 // uncomment the line below and tell us what the context of "this" is when we invoke vacation.whatIsThis
-//let context3 = ???
+let context3 = vacation;
 
 
 class Family {
@@ -116,4 +116,4 @@ class Family {
 let family1 = new Family(2, 4, 1)
 
 // uncomment the line below and tell us what the context of "this" is for the instance of Family created above.
-//let context4 = ???
+let context4 = family1;
